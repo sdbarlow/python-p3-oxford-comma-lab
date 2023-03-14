956889d0f933e7e56ba2f7b5a354f468b5b326ff
sdbarlow <sethdbarlow@gmail.com>
2023-03-14 10:23:40 -0600

    all tests pass

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ''.join(items)
+    elif len(items) == 2:
+        return ' and '.join(items)
+    elif len(items) == 3:
+        return (', '.join(items[0:2]) + ", and " + (items[2]))
+    elif len(items) > 3:
+        return (', '.join(items[0:-1]) + ", and " + (items[-1]))
+    else:
+        print("broken")
